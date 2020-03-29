23a73743e5005ba742df62755819c6c84003a94e
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-03-29 13:14:01 +0530

    sorting objects using .sort

--- 35. sorted_functions.py
+++ 35. sorted_functions.py
@@ -1,4 +1,5 @@
 from student import Student
+from operator import itemgetter, attrgetter
 
 # sorting a dictionary
 dict = {
@@ -21,6 +22,7 @@ student_tuples = [
     ('dave', 'B', 10),
 ]
 print("\ntuple sorted based on 2nd index: " + str(sorted(student_tuples, key=lambda student: student[2])))
+print("\ntuple sorted based on 0th index: " + str(sorted(student_tuples, key=itemgetter(0))))
 
 
 # sorting a tuple based on second element
@@ -36,15 +38,28 @@ random = [
 ]
 print("\nsorted list: " + str(sorted(random, key=take_second)))
 
-# to check if Student class is working
-student1 = Student("john", "Physics", 3, True)
-print(student1.person)
-
 # sorting an object
 student_objects = [
-    student1,
+    Student("john", "Physics", 3, True),
     Student('jane', 'Chemistry', 3.5, False),
     Student('dave', 'Math', 2.9, False)
 ]
-print("\n" + str(student1.major))
-sorted(student_objects, key=lambda a: a.gpa)
+
+# sorted(student_objects, key=lambda b: b.gpa)
+# print("\n")
+# for student in student_objects:
+#     print("GPA of " + str(student.person) + " is: " + str(student.gpa))
+# print("\n")
+# sorted(student_objects, key=attrgetter('gpa'))
+# for student in student_objects:
+#     print("GPA of " + str(student.person) + " is: " + str(student.gpa))
+print("-----------------------------------")
+student_objects.sort(key=lambda a: a.gpa)
+# on large list you will get better performance using attrgetter
+# as your key. this is just an optimised (lower lever) form of
+# lambda function
+student_objects.sort(key=attrgetter('gpa'), reverse=True)
+
+
+for student in student_objects:
+    print("GPA of " + str(student.person) + " is: " + str(student.gpa))
