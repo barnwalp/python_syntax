aa30de821c3c4f3220070095e77faba25482aafd
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-04-04 14:15:20 +0530

    using staticmethod

--- oop_class_static_method.py
+++ oop_class_static_method.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Employee:
     num_of_emps = 0
     raise_amt = 1.04
@@ -26,20 +29,33 @@ class Employee:
         first, last, pay = emp_str.split('-')
         return cls(first, last, pay)
 
+    # method should be a static method if you don't access the
+    # instance for the class anywhere within the method
+    @staticmethod
+    def is_workday(day):
+        # weekday in python return number for given day for example
+        # sunday and saturday are 6  & 5
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
 emp_1 = Employee('Pankaj', 'Barnwal', 60000)
 emp_2 = Employee('Kumari', 'Payal', 60000)
 
 # way to change the class variable
 Employee.set_raise_amt(1.05)
-
 print(Employee.raise_amt)
-print(emp_1.raise_amt)
-print(emp_2.raise_amt)
 
-# using classmethod as alternative constructor
+# using class method as alternative constructor
 emp_str_1 = "John-Doe-70000"
 emp_str_2 = "Steve-Smith-40000"
 emp_str_3 = "Jane-Doe-50000"
 
 new_emp_1 = Employee.from_string(emp_str_1)
 print(new_emp_1.email)
+
+print("\n--------------------------")
+
+my_day = datetime.date(2020, 4, 4)
+print(Employee.is_workday(my_day))
