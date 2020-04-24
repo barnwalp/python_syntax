9b4bd6a0e732d3572e198863e3843d4c9e4508ca
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-04-24 12:50:46 +0530

    feat - added __str__ function

--- 38. graph_as_class.py
+++ 38. graph_as_class.py
@@ -6,12 +6,13 @@ class Graph(object):
 
         """
         names with a leading underscore are to indicate that the attrubute or
-        method is intended to be private. from M import * does not import objects
-        whose name starts with an underscore.
+        method is intended to be private. from M import * does not import
+        objects whose name starts with an underscore.
 
-        Any identifier of the form __spam with at least two leading underscores,
-        at most one trailing underscore is textually replaced with _classname__spam
-        so it can be used to define class-private instances and class variables
+        Any identifier of the form __spam with at least two leading
+        underscores, at most one trailing underscore is textually replaced
+        with _classname__spam so it can be used to define class-private
+        instances and class variables.
 
         """
         self.__graph_dict = graph_dict
@@ -26,6 +27,34 @@ class Graph(object):
         if vertex not in self.__graph_dict:
             self.__graph_dict[vertex] = []
 
+    def add_edge(self, edge):
+        edge = set(edge)
+
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        edges = []
+        for vertex, value in self.__graph_dict.items():
+            for neighbour in value:
+                # {} will ensure that orientation of edges
+                # does not matter
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self.__graph_dict:
+            res += str(k) + " "
+        res += "\nedges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res
+
 
 g = {'a': ['d'],
      'b': ['c'],
@@ -37,3 +66,5 @@ g = {'a': ['d'],
 
 graph = Graph(g)
 print(f'vertices of graph: {graph.vertices()}')
+print(f'edges of graph: {graph.edges()}')
+print(f'\n{graph}')
