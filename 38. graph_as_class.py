551df7a9b8b7c3b82ece031d30d602bc3eda854d
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-04-24 07:38:02 +0530

    feat - added add_edge feature in graph

--- 38. graph_as_class.py
+++ 38. graph_as_class.py
@@ -1,13 +1,18 @@
 class Graph(object):
+    # if no dictionary is given, an empty dictionary will be used
     def __init__(self, graph_dict=None):
         if graph_dict is None:
             graph_dict = {}
 
         """
-        __graph_dict is textually replaced with _Graph__graph_dict
-        where Graph is the current class. this is intended to give
-        classes and easy way to define private instance variables &
-        methods
+        names with a leading underscore are to indicate that the attrubute or
+        method is intended to be private. from M import * does not import objects
+        whose name starts with an underscore.
+
+        Any identifier of the form __spam with at least two leading underscores,
+        at most one trailing underscore is textually replaced with _classname__spam
+        so it can be used to define class-private instances and class variables
+
         """
         self.__graph_dict = graph_dict
 
@@ -20,3 +25,15 @@ class Graph(object):
     def add_vertex(self, vertex):
         if vertex not in self.__graph_dict:
             self.__graph_dict[vertex] = []
+
+
+g = {'a': ['d'],
+     'b': ['c'],
+     'c': ['b', 'c', 'd', 'e'],
+     'd': ['a', 'c'],
+     'e': ['c'],
+     'f': []
+     }
+
+graph = Graph(g)
+print(f'vertices of graph: {graph.vertices()}')
