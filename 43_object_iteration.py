8f35b12724dfc67aab99214e79ffbd9d4e5bf668
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-04-26 21:50:32 +0530

    feat - basic of iter & next

--- 43_object_iteration.py
+++ 43_object_iteration.py
@@ -1,3 +1,36 @@
+"""
+for an object to be iterable, it must have an __iter__() method. for example
+list is an iterable but not an iterator, but if we run an __iter__method on
+the list, it will return an iterator.
+"""
+nums = [1, 2, 3]
+
+i_nums = nums.__iter__()    # iter(num) is same
+
+dir(nums)       # will not have __next__()
+dir(i_nums)     # will have __next__()
+
+# print(next(i_nums))
+# print(next(i_nums))     # it remembers the state
+# print(next(i_nums))
+# print(next(i_nums))     # will raise error as there are only 3 value
+
+
+# for loop basically gets an iterator of original object like we did on line 8
+# and then it's getting the next value until it hits a stop iteration exception
+# Iterator is an object with a state so that it remembers where it is during
+# iteration. Basically it is doing something like this
+
+while True:
+    try:
+        item = next(i_nums)
+        print(item)
+    except StopIteration:
+        break
+
+# Iterator can only go forward
+
+
 class Tailer:
     def __init__(self, cloth_type, style, measurements=None):
         self.cloth_type = cloth_type
