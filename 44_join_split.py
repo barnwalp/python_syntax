1b1846d840304a70ff0b29d8a29b1ba07da85ee4
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-05-01 09:46:23 +0530

    feat - more examples of join method

--- 44_join_split.py
+++ 44_join_split.py
@@ -6,9 +6,19 @@ print(user_info)
 name, age = 'pankaj 29'.split()
 print(f'{name} --> {age}')
 
-# join method - converts list to string
+# join method - converts iterables such as list, tuple
+# string, dictionary and set to a string
 client_info = ['Prashasti', '26']
 
-# in the inverted comma, use character with which items
+# in the inverted comma, use string with which items
 # need to be joined
 print(' - '.join(client_info))
+
+# this takes format of string.join(iterable)
+string_1 = 'pankaj'
+string_2 = '123'
+print(string_1.join(string_2))
+
+test = {'python', 'java', 'javascript'}
+s = '->->->'
+print(s.join(test))
