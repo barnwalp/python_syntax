58930308392278e3d98bbc4f76e3895ba6ea80a0
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-05-05 07:20:08 +0530

    feat - more functions of sets

--- 42. set.py
+++ 42. set.py
@@ -15,20 +15,34 @@ print(my_set)
 my_set.add('d')
 print(f'set after adding data: {my_set}')
 
-my_set_1 = {'Richa', 'Arti', 'Archana'}
+# Adding multiple value in set
+new_set = {'g', 'h'}
+my_set.update('e', 'f', new_set)
+print(f'set after adding data: {my_set}')
 
+# Remove a value from the set
+my_set.remove('a')
+# discard does not throw an error like remove when the key is not in the set
+my_set.discard('n')
+
+my_set_1 = {'Richa', 'Arti', 'Archana'}
 my_set_2 = {'jay', 'idri', 'Arti'}
+my_set_3 = {'Lisa', 'Debu', 'Arti'}
 
 # time complexit of union is O(len(s1) + len(s2)), s1,s2 are set
 master_set = my_set_1.union(my_set_2)
 print(f'union of set1 and set2 is: {master_set}')
 
 # time complexity of intersection is O(min(len(s1), len(s2)))
-my_set_3 = my_set_1.intersection(my_set_2)
-print(f'intersection of set1 and set2 is: {my_set_3}')
+my_set_4 = my_set_1.intersection(my_set_2, my_set_3)
+print(f'intersection of set1 and set2 is: {my_set_4}')
+
+# difference of set
+print(f' elements in my_set_1 but not in my_set_2: {(my_set_1 - my_set_2) - my_set_3}')
+print(my_set_1.difference(my_set_2, my_set_3))
 
 """
-key in s	       containment check
+key in s	       containment check           T(n) = O(1)
 key not in s	   non-containment check
 s1 == s2	       s1 is equivalent to s2
 s1 != s2	       s1 is not equivalent to s2
