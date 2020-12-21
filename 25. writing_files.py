323e7ed54f66561767671a687969031963dc9fcc
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-21 18:02:59 +0530

    chore/moved all files in separate folders

--- 25. writing_files.py
+++
@@ -1,9 +0,0 @@
-
-employee_file = open("employee.txt", "a")
-# employee_file.write("\nHassan - Retail Sales")
-employee_file.close()
-
-employee_file = open("index.html", "w")
-employee_file.write("<p>This is HTML<p>")
-employee_file.close()
-
