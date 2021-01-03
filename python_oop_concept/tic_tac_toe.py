e0f0e8f8144d37c145e387086b1d34eccebeb2c6
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-01-03 19:57:43 +0530

    feat/implemented compmove and playermove

--- python_oop_concept/tic_tac_toe.py
+++ python_oop_concept/tic_tac_toe.py
@@ -1,10 +1,13 @@
 board = [' ' for x in range(10)]
 
-def insertLetter(letter, pos):
+def insertBoard(letter, pos):
+    global board
     board[pos] = letter
+
 
 def spaceIsFree(pos):
     return board[pos] == ' '
+
 
 def printBoard(board):
     print('   |   |')
@@ -19,6 +22,7 @@ def printBoard(board):
     print(' ' +board[7] + ' | ' +board[8] + ' | ' +board[9])
     print('   |   |')
 
+
 # bo is board and le is letter
 def isWinner(bo, le):
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or
@@ -30,14 +34,69 @@ def isWinner(bo, le):
     (bo[1] == le and bo[5] == le and bo[9] == le) or
     (bo[3] == le and bo[5] == le and bo[7] == le))
 
+
 def playerMove():
-    pass
+    run = True
+    while run:
+        move = input('Please select a position to place an X (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertBoard('x', move)
+                else:
+                    print('This position is already occupied')
+            else:
+                print('Please type a number within a range')
+        except:
+            print('Please type a number')
+
 
 def compMove():
-    pass
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x!= 0]
+    move = 0
+    # check for possible winning move to take or block opponents winning move
+    for let in ['o', 'x']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
 
-def selectRandom(board):
-    pass
+    # try to take one of the corners
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    # Try to take the center
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    # Take any edge
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 5, 8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+
+    return move
+
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
+
 
 def isBoardFull(board):
     if board.count(' ') > 1:
@@ -45,10 +104,10 @@ def isBoardFull(board):
     else:
         return False
 
+
 def main():
     print('Welcome to tic tac toe')
     printBoard()
-
     while not(isBoardFull(board)):
         if not(isWinner(board, 'o')):
             playerMove()
@@ -67,3 +126,5 @@ def main():
         print('Tie Game')
 
 main()
+
+
