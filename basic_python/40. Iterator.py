f2eae2ef6a41c6ddf1212f1057861765c84111dd
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-03-20 17:59:51 +0530

    feat/iterator and iterables

--- basic_python/40. Iterator.py
+++ basic_python/40. Iterator.py
@@ -1,37 +1,21 @@
-'''
-Iterator is an object that contains a countable number of values, meaning that
-you can traverse through all the value. In python, iterator is an object which
-implements the iterator protocol, which consists of the methods __iter__()
-and __next__()
+# list is iterable but not an iterator
+# iterable is something that can be looped over, such as list, tuple, string
+# generator, files, dictionaries etc
 
-Lists, tuples, dictionary, strings and sets are all iterable object. All these objects
-have a iter() method, which is used to get an iterator.
+# An Iterator is an object with a state so that it remembers where it is
+# while iteration. calling an iter() method on a iterable returns an iterator
 
-iter() method is used to convert an iterable to iterator. iter() uses next()
-for accessing value. it takes two parameter; first is the iterable and the
-2nd is the sentinel which is a special value that is used to represent the
-end of a sequence.
-
-without sentinel, first object must support iteration protocol. it setinel
-is provided, then object must be a callable object. The iterator created in
-this case will call object with no arguments for each call to its __next__()
-method; if the value returned is equal to sentinel, StopIteration will be raised
-
-iteration object remembers iteration count via internal count variable.
-once iteration is complete, it raises a Stopiteration exception so it can
-be used to traverse the container just once.
-'''
+# while an iterable only has an __iter__() method, iterator has both __iter__()
+# and __next__() method
 
 my_tuple = ('apple', 'banana', 'cherry')
 my_iter = iter(my_tuple)
-# my_iter is and iterator of my_tuple, which is an iterable.
-# print(next(my_iter))
-# print(next(my_iter))
-# print(next(my_iter))
-# print('-----------')
 
-for data in my_tuple:
-    print(data)
+# my_iter is and iterator of my_tuple, which is an iterable.
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print('-----------')
 
 
 class PrintNumber:
