be473443baa77ccc8c646d6343b6a48c017da08d
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-08 17:03:20 +0530

    feat/creating and reading files in python

--- basic_python/46_read_write_file.py
+++ basic_python/46_read_write_file.py
@@ -1,20 +1,19 @@
-file = open('test.txt', 'r')
+"""
+6 access mode for file IO:
+1. Read only('r'): Handle is at the begining of the file.
+2. Read and write('r+'): Handle is at the begining of the file.
+3. Write only('w'):open the file and overwrite. file is created if it does not exist
+4. Append only('a'): Handle is at the end of the file.
+5. Append and Read('a+'): Handle is at the end. file is created if does not exist.
+6. Write and Read('w+'): file is overwritten. 
+"""
 
-f = file.readlines()
+# Creating a file
+with open('new.txt', 'w') as f:
+    content = 'This is some random text\nmore random stuff'
+    f.write(content)
 
-newList = []
-for line in f:
-    # this if-else statement can  be replaced by using
-    # newList.append(line.strip())
-    if line[-1] == '\n':
-        newList.append(line[:-1])
-    else:
-        newList.append(line)
-print(newList)
-
-file_2 = open('file.txt', 'w')
-
-for item in newList:
-    file_2.write(item + '\n')
-
-file.close()
+# Reading a file
+with open('new.txt', 'r') as f:
+    for line in f.readlines():
+        print(line)
