f4bc24fff24ce2cb1bd1a743010ed721e6c3767e
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-12 07:59:56 +0530

    feat/adding data to database table

---
+++ sql_database/database.py
@@ -0,0 +1,39 @@
+from mysql.connector import connect
+import os
+
+# connecting to mysql database
+mydb = connect(
+    host='127.0.0.1',
+    user='root',
+    passwd=os.environ.get('sql_password'),
+    database='testdb'
+)
+
+mycursor = mydb.cursor()
+# Creating a table with 2 variables
+# mycursor.execute("CREATE TABLE students (name VARCHAR(256), age INTEGER(10))")
+# mycursor.execute("SHOW TABLES")
+
+# for tb in mycursor:
+#     print(tb)
+
+# Writing a sql formula; (%s %s) is a placeholder
+sqlFormula = (
+    "INSERT INTO students (name, age)"
+    "VALUES (%s, %s)"
+)
+data = [
+    ("Adaa", 32),
+    ("Anita", 40),
+    ("Srinidhi", 28),
+    ("Karishma", 37),
+    ("Surbhi", 32),
+    ("Sonarika", 28),
+    ("Nia", 30),
+    ("Erica", 28)
+    ]
+
+# Executing the formula in sql with the tuple
+mycursor.executemany(sqlFormula, data)
+# Saving the databases
+mydb.commit()
