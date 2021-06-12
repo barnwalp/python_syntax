b58653c79f4cca135826d5665d87a39cfd1a720a
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-12 18:10:41 +0530

    feat/creating dom using fromstring

--- basic_python/52_reading_xml.py
+++ basic_python/52_reading_xml.py
@@ -16,5 +16,45 @@ books = tree.findall('book')
 authors = tree.findall('book/author')
 
 # getting all the author name
-for authors in tree.findall('book/author'):
-    print(authors.text)
+# for authors in tree.findall('book/author'):
+#     print(authors.text)
+
+# Reading xml data from a variable
+xml_data = """
+    <?xml version="1.0"?>
+    <catalog>
+        <book id="bk101">
+            <author>Gambardella, Matthew</author>
+            <title>XML Developer's Guide</title>
+            <genre>Computer</genre>
+            <price>44.95</price>
+            <publish_date>2000-10-01</publish_date>
+            <description>An in-depth look at creating applications 
+            with XML.</description>
+        </book>
+        <book id="bk102">
+            <author>Ralls, Kim</author>
+            <title>Midnight Rain</title>
+            <genre>Fantasy</genre>
+            <price>5.95</price>
+            <publish_date>2000-12-16</publish_date>
+            <description>A former architect battles corporate zombies, 
+            an evil sorceress, and her own childhood to become queen 
+            of the world.</description>
+        </book>
+        <book id="bk103">
+            <author>Corets, Eva</author>
+            <title>Maeve Ascendant</title>
+            <genre>Fantasy</genre>
+            <price>5.95</price>
+            <publish_date>2000-11-17</publish_date>
+            <description>After the collapse of a nanotechnology 
+            society in England, the young survivors lay the 
+            foundation for a new society.</description>
+        </book>
+    </catalog>
+"""
+print(type(xml_data))
+dom = ET.fromstring(xml_data)
+# for value in dom.findall('book/prices'):
+#     print(value.text)
